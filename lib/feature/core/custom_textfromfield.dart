1e176642eb464497ac6ed89c34f90cac52f8a11a
Zain <muhammadzainidrees@gmail.com>
2026-04-27 18:59:48 +0500

    folder

---
+++ lib/feature/core/custom_textfromfield.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CustomTextfromfield extends StatefulWidget {
+  const CustomTextfromfield({super.key});
+
+  @override
+  State<CustomTextfromfield> createState() => _CustomTextfromfieldState();
+}
+
+class _CustomTextfromfieldState extends State<CustomTextfromfield> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Color(0xffF3F3FE),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        disabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: Color(0xffF3F3FE)),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: Color(0xffF3F3FE)),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: Color(0xffF3F3FE)),
+        ),
+      ),
+    );
+  }
+}
