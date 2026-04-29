d73af8778aacbe34f26e495039764f15bb57dc40
muhammadarqamattar <muhammadarqamattar@gmail.com>
2026-04-28 22:00:24 -0900

    new signup screen and oyher changing

--- lib/feature/signup/view/screen/signup_screen.dart
+++ lib/feature/signup/view/screen/signup_screen.dart
@@ -1,4 +1,8 @@
+import 'package:clarity/feature/core/custom_textfromfield.dart';
 import 'package:clarity/widgets/custom_container.dart';
+import 'package:clarity/widgets/custom_elevatedbutton.dart';
+import 'package:clarity/widgets/custom_elevatedbuttonicon.dart';
+import 'package:clarity/widgets/custom_securitycontainer.dart';
 import 'package:flutter/material.dart';
 
 class SignupScreen extends StatefulWidget {
@@ -12,11 +16,13 @@ class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
+      body: SingleChildScrollView(
         padding: EdgeInsets.all(16.0),
         child: Column(
           children: [
+            SizedBox(height: 20),
             Row(
+              spacing: 10,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
@@ -43,9 +49,67 @@ class _SignupScreenState extends State<SignupScreen> {
             ),
             SizedBox(height: 20),
             CustomContainer(
-              height: 589,
+              height: 595,
               width: 350,
-              child: Column(children: []),
+              child: Column(
+                spacing: 40,
+                children: [
+                  CustomTextfromfield(title: 'Full Name'),
+                  CustomTextfromfield(title: 'Email Address'),
+                  CustomTextfromfield(title: 'Password'),
+                  CustomTextfromfield(title: 'Confirm Password'),
+                  Column(
+                    spacing: 20,
+                    children: [
+                      CustomElevatedbutton(title: 'Sign Up', width: 300),
+                      Row(
+                        spacing: 10,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 1,
+                            width: 129,
+                            decoration: BoxDecoration(color: Color(0XFFC3C6D7)),
+                          ),
+                          Text(
+                            'OR',
+                            style: TextStyle(
+                              color: Color(0XFF737686),
+                              fontSize: 12,
+                              fontWeight: FontWeight(600),
+                            ),
+                          ),
+                          Container(
+                            height: 1,
+                            width: 129,
+                            decoration: BoxDecoration(color: Color(0XFFC3C6D7)),
+                          ),
+                        ],
+                      ),
+
+                      Customelevatedbuttonicon(),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            Text(
+              'Already have an account? Sign in',
+              style: TextStyle(
+                color: Color(0XFF434655),
+                fontSize: 14,
+                fontWeight: FontWeight(400),
+              ),
+            ),
+            SizedBox(height: 20),
+            CustomSecuritycontainer(
+              icon: Icons.security,
+              title:
+                  'Your data is encrypted and stored'
+                  'securely. We never share your personal'
+                  'information with third parties.',
+              color: Color(0XFFD0E1FB),
             ),
           ],
         ),
