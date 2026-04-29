af2403515b2f266fafbba849cd4938cf5b8bc16e
Zain <muhammadzainidrees@gmail.com>
2026-04-29 11:25:39 +0500

    MAKING SIGNIN SCREEN

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:clarity/feature/signin/screen/signin_screen.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
